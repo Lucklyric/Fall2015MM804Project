c31f4d513214eab65e7b23e17f11832d38cd5e32
Lucklyric <xinyao1@ualberta.ca>
2015-11-26 18:12:46 -0700

    1. Load pinchSnap

--- MatLabScripts/initScript.m
+++ MatLabScripts/initScript.m
@@ -2,6 +2,6 @@ clear;
 tic;
 fprintf('Start loading json file...\n');
 startT = toc;
-rawMotion = loadjson('../MotionData/RealHand/pinchData.json');
+realPinchSnap= loadjson('../MotionData/RealHand/pinchSnap.json');
 finishT = toc;
 fprintf('Load finished costs %f seconds\n',(finishT-startT));
