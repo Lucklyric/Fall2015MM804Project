a0999b481d6170468a6732841138145c36bb6f02
Lucklyric <xinyao1@ualberta.ca>
2015-11-26 20:31:40 -0700

    1. Init update

--- MatLabScripts/initScript.m
+++ MatLabScripts/initScript.m
@@ -1,7 +1,9 @@
-clear;
-tic;
-fprintf('Start loading json file...\n');
-startT = toc;
-realPinchSnap= loadjson('../MotionData/RealHand/pinchSnap.json');
-finishT = toc;
-fprintf('Load finished costs %f seconds\n',(finishT-startT));
+% clear;
+% tic;
+% fprintf('Start loading json file...\n');
+% startT = toc;
+% realPinchSnap= loadjson('../MotionData/RealHand/pinchSnap.json');
+% finishT = toc;
+% fprintf('Load finished costs %f seconds\n',(finishT-startT));
+load('../MotionData/RealHand/pinchSnap.mat');
+load('../MotionData/RealHand/pinchData.mat');
